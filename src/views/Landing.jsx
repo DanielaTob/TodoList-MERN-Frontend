3a0236de737d22c618545236864b41fc344266c5
daniela <moreno.tobar.daniela@gmail.com>
2022-04-05 20:06:14 -0500

    remove lorem

--- src/views/Landing.jsx
+++ src/views/Landing.jsx
@@ -18,9 +18,10 @@ export default function Landing() {
             <div>
               <h1 className="text-5xl text-white font-bold">Smart Task!</h1>
               <p className="py-6 text-white">
-                Provident cupiditate voluptatem et in. Quaerat fugiat ut
-                assumenda excepturi exercitationem quasi. In deleniti eaque aut
-                repudiandae et a id nisi.
+              Regain your clarity and peace of mind by taking all those tasks out of your 
+              head to put on your Smart Task!, no matter where you are.
+              Smart Task! gives you the peace of mind of knowing that everything is properly organized
+              and registered so that you can progress in the things that are really important to you.
               </p>
               <Link to="/Tasks">
               <button className="btn btn-info text-white">Get Started</button>
